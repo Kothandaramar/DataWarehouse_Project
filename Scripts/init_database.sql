4c890f54cada7d1c3484d97ecbe2012724092226
Kothandaramar Sivakumar <87431656+Kothandaramar@users.noreply.github.com>
2025-07-29 23:15:54 +0530

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,8 @@
+-- Create the 3 Sections of the data warehouse ie the Bronze , Silver , Gold
+-- Create the DataWrehouse.
+CREATE DATABASE IF NOT EXISTS DataWarehouse_Bronze;
+CREATE DATABASE IF NOT EXISTS DataWarehouse_Silver;
+CREATE DATABASE IF NOT EXISTS DataWarehouse_Gold;
+
+-- run this to use the respective databases.
+-- use datawarehouse <database name> ;
